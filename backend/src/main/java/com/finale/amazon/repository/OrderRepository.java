715b86b6277b1a4ccea3904be4cad2bba12f2a3c
dimamironuk <dimamironuk2019@gmail.com>
2025-09-13 11:25:42 +0300

    #22 - Added review editing and displaying all reviews for a specific product

--- backend/src/main/java/com/finale/amazon/repository/OrderRepository.java
+++ backend/src/main/java/com/finale/amazon/repository/OrderRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.finale.amazon.entity.Order;
+import com.finale.amazon.entity.Review;
 import com.finale.amazon.entity.User;
 
 @Repository
@@ -28,6 +29,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByOrderStatus_NameIn(List<String> statusNames);
 
     List<Order> findByUser(User user);
-
+    
     List<Order> findByOrderStatus_NameInAndUserId(List<String> statusNames, Long userId);
+
+
 }
