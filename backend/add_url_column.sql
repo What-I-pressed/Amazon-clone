58a37767af1cd6620b1499ddc9acb5600b216fc8
Petyna <157280448+Petyna@users.noreply.github.com>
2025-09-28 17:27:49 +0300

    Delete backend/add_url_column.sql

--- backend/add_url_column.sql
+++
@@ -1,7 +0,0 @@
--- Add url column to users table if it doesn't exist
-ALTER TABLE users ADD COLUMN IF NOT EXISTS url VARCHAR(512);
-
--- Create uploads directories (you'll need to create these manually on the filesystem)
--- mkdir uploads
--- mkdir uploads/avatars
--- mkdir uploads/pictures (if not already exists)
